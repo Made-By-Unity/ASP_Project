c9700f1ae67b116218793eb315b26c838b8b1e98
khi1601 <tgkim1601@gmail.com>
2023-05-08 11:29:08 +0900

    다중 클라이언트 연결

--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -21,57 +22,62 @@ namespace Server
     {
         TcpListener server = null;
         TcpClient client = null;
-        static int counter = 0;
+        Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>();
+        static uint num = 0;
+
+
         public Form1()
         {
             InitializeComponent();
-            // socket start
-            Thread t = new Thread(InitSocket);
-            t.IsBackground = true;
-            t.Start();
+            Thread thr = new Thread(Init);
+            thr.IsBackground = true;
+            thr.Start();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private void InitSocket()
+
+        private void Init()
         {
-            server = new TcpListener(IPAddress.Any, 9999);
+            server = new TcpListener(IPAddress.Any, 5417);
             client = default(TcpClient);
             server.Start();
-            DisplayText(">> Server Started");
+            ShowMessage("서버 오픈");
 
             while (true)
             {
-                try
-                {
-                    counter++;
-                    client = server.AcceptTcpClient();
-                    DisplayText(">> Accept connection from client");
+                num++;
+                client = server.AcceptTcpClient();
+                ShowMessage("> Connection From Client");
 
-                    handleClient h_client = new handleClient();
-                    h_client.OnReceived += new handleClient.MessageDisplayHandler(DisplayText);
-                    h_client.decreaseCount += new handleClient.ClientCounter(DecreaseCount);
-                    h_client.startClient(client, counter);
-                }
-                catch (SocketException se)
-                {
-                    Trace.WriteLine(string.Format("InitSocket - SocketException : {0}", se.Message));
-                }
-                catch (Exception ex)
-                {
-                    Trace.WriteLine(string.Format("InitSocket - Exception : {0}", ex.Message));
-                }
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                string nickName = Encoding.Unicode.GetString(buffer, 0, bytes);
+                nickName = nickName.Substring(0, nickName.IndexOf("$"));
+                clientList.Add(client, nickName);
+                SendMessageAll(nickName + " 님이 참여하셨습니다. ", "", false);
+                CClient c_Client = new CClient();
+                c_Client.EDisconnected += new CClient.DisconnectedHandler(Disconnected);
+                c_Client.EReceived += new CClient.ShowTextHandler(OnReceived);
+                c_Client.StartClient(client, clientList);
             }
+            client.Close();
+            server.Stop();
         }
-
-        private void DecreaseCount()
+        private void OnReceived(string msg, string nickName)
         {
-            counter--;
+            string str = "From client " + nickName + " : " + msg;
+            ShowMessage(str);
+            SendMessageAll(msg, nickName, true);
         }
-
-        private void DisplayText(string text)
+        private void Disconnected(TcpClient clientSocket)
+        {
+            if (clientList.ContainsKey(clientSocket))
+                clientList.Remove(clientSocket);
+        }
+        private void ShowMessage(string text)
         {
             if (textBox1.InvokeRequired)
             {
@@ -84,106 +90,84 @@ namespace Server
                 textBox1.AppendText(text + Environment.NewLine);
         }
 
-        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        public void SendMessageAll(string msg, string nickName, bool b)
         {
-            if (client != null)
+            foreach (KeyValuePair<TcpClient, string> pair in clientList)
             {
-                client.Close();
-                client = null;
-            }
-
-            if (server != null)
-            {
-                server.Stop();
-                server = null;
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream nStream = client.GetStream();
+                byte[] buff = null;
+                if (b)
+                {
+                    buff = Encoding.Unicode.GetBytes(nickName + " : " + msg);
+                }
+                else
+                {
+                    buff = Encoding.Unicode.GetBytes(msg);
+                }
+                nStream.Write(buff, 0, buff.Length);
+                nStream.Flush();
             }
         }
     }
-}
-class handleClient
-{
-    TcpClient clientSocket;
-    int i_ClientNumber;
 
-    public void startClient(TcpClient ClientSocket, int clientNo)
+
+    class CClient
     {
-        this.clientSocket = ClientSocket;
-        this.i_ClientNumber = clientNo;
-
-        Thread t_hanlder = new Thread(Chat);
-        t_hanlder.IsBackground = true;
-        t_hanlder.Start();
-    }
-
-    public delegate void MessageDisplayHandler(string text);
-    public event MessageDisplayHandler OnReceived;
-
-    public delegate void ClientCounter();
-    public event ClientCounter decreaseCount;
-
-    private void Chat()
-    {
-        NetworkStream stream = null;
-        try
+        TcpClient clientSocket = null;
+        public Dictionary<TcpClient, string> clientList = null;
+        public delegate void DisconnectedHandler(TcpClient clientSocket);
+        public event DisconnectedHandler EDisconnected;
+        public delegate void ShowTextHandler(string text, string nickName);
+        public event ShowTextHandler EReceived;
+        public void StartClient(TcpClient socket, Dictionary<TcpClient, string> List)
         {
-            byte[] buffer = new byte[1024];
-            string msg = string.Empty;
-            int bytes = 0;
-            int MessageCount = 0;
+            clientSocket = socket;
+            clientList = List;
+            Thread t = new Thread(ChattingSystem);
+            t.IsBackground = true;
+            t.Start();
+        }
 
-            while (true)
+        private void ChattingSystem()
+        {
+            NetworkStream nStream = null;
+            try
             {
-                MessageCount++;
-                stream = clientSocket.GetStream();
-                bytes = stream.Read(buffer, 0, buffer.Length);
-                msg = Encoding.Unicode.GetString(buffer, 0, bytes);
-                msg = msg.Substring(0, msg.IndexOf("$"));
-                msg = "Data Received : " + msg;
-
-                if (OnReceived != null)
-                    OnReceived(msg);
-
-                msg = "Server to client(" + i_ClientNumber.ToString() + ") " + MessageCount.ToString();
-                if (OnReceived != null)
-                    OnReceived(msg);
-
-                byte[] sbuffer = Encoding.Unicode.GetBytes(msg);
-                stream.Write(sbuffer, 0, sbuffer.Length);
-                stream.Flush();
-
-                msg = " >> " + msg;
-                if (OnReceived != null)
+                byte[] buff = new byte[1024];
+                string msg = string.Empty;
+                int bytes = 0;
+                while (true)
                 {
-                    OnReceived(msg);
-                    OnReceived("");
+                    nStream = clientSocket.GetStream();
+                    bytes = nStream.Read(buff, 0, buff.Length);
+                    msg = Encoding.Unicode.GetString(buff, 0, bytes);
+                    msg = msg.Substring(0, msg.IndexOf("$"));
+                    if (EReceived != null)
+                        EReceived(msg, clientList[clientSocket].ToString());
                 }
             }
-        }
-        catch (SocketException se)
-        {
-            Trace.WriteLine(string.Format("doChat - SocketException : {0}", se.Message));
-
-            if (clientSocket != null)
+            catch (SocketException e)
             {
-                clientSocket.Close();
-                stream.Close();
+                if (clientSocket != null)
+                {
+                    if (EDisconnected != null)
+                        EDisconnected(clientSocket);
+
+                    clientSocket.Close();
+                    nStream.Close();
+                }
             }
-
-            if (decreaseCount != null)
-                decreaseCount();
-        }
-        catch (Exception ex)
-        {
-            Trace.WriteLine(string.Format("doChat - Exception : {0}", ex.Message));
-
-            if (clientSocket != null)
+            catch (Exception e)
             {
-                clientSocket.Close();
-                stream.Close();
+                if (clientSocket != null)
+                {
+                    if (EDisconnected != null)
+                        EDisconnected(clientSocket);
+                    clientSocket.Close();
+                    nStream.Close();
+                }
             }
-
-            if (decreaseCount != null)
-                decreaseCount();
         }
     }
 }
