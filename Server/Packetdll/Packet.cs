7ac8c11935b6a9fa1502c37f1b48e18a8948fd3c
khi1601 <tgkim1601@gmail.com>
2023-05-12 16:18:16 +0900

    Update Packet.cs

--- Server/Packetdll/Packet.cs
+++ Server/Packetdll/Packet.cs
@@ -7,7 +7,62 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 namespace Packetdll
 {
+    public enum PacketType : int
+    {
+        Login = 0,
+        Login_RESULT,
+        Member_REGISTER,
+        Member_REGISTER_RESULT
+    }
     public class Packet
     {
+        public int packet_Type;
+        public int packet_Length;
+
+        public Packet()
+        {
+            this.packet_Type = 0;
+            this.packet_Length = 0;
+        }
+        public static byte[] Serialize(Object data)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream(1024 * 4); // packet size will be maximum 4k
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(ms, data);
+                return ms.ToArray();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Object Deserialize(byte[] data)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream(1024 * 4);
+                ms.Write(data, 0, data.Length);
+
+                ms.Position = 0;
+                BinaryFormatter bf = new BinaryFormatter();
+                Object obj = bf.Deserialize(ms);
+                ms.Close();
+                return obj;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+    [Serializable]
+    public class Login : Packet
+    {
+        public string id_str { get; set; }
+        public string pw_str { get; set; }
+
     }
 }
