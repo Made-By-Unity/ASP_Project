6250a2ec57f0a1d50656fa6ac8a96755d50286a4
Jaehyun <96245416+Firecrackre@users.noreply.github.com>
2023-06-14 14:26:25 +0900

    로비 디자인 업데이트

--- Server/Client/Room/Lobby.Designer.cs
+++ Server/Client/Room/Lobby.Designer.cs
@@ -38,75 +38,87 @@
             // 
             // tbPlayer1
             // 
-            this.tbPlayer1.Location = new System.Drawing.Point(110, 69);
-            this.tbPlayer1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tbPlayer1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbPlayer1.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPlayer1.Location = new System.Drawing.Point(59, 34);
+            this.tbPlayer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbPlayer1.Name = "tbPlayer1";
             this.tbPlayer1.ReadOnly = true;
-            this.tbPlayer1.Size = new System.Drawing.Size(206, 35);
+            this.tbPlayer1.Size = new System.Drawing.Size(113, 27);
             this.tbPlayer1.TabIndex = 0;
             // 
             // tbPlayer2
             // 
-            this.tbPlayer2.Location = new System.Drawing.Point(110, 148);
-            this.tbPlayer2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tbPlayer2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbPlayer2.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPlayer2.Location = new System.Drawing.Point(59, 74);
+            this.tbPlayer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbPlayer2.Name = "tbPlayer2";
             this.tbPlayer2.ReadOnly = true;
-            this.tbPlayer2.Size = new System.Drawing.Size(206, 35);
+            this.tbPlayer2.Size = new System.Drawing.Size(113, 27);
             this.tbPlayer2.TabIndex = 1;
             // 
             // tbPlayer3
             // 
-            this.tbPlayer3.Location = new System.Drawing.Point(110, 228);
-            this.tbPlayer3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tbPlayer3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbPlayer3.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPlayer3.Location = new System.Drawing.Point(59, 114);
+            this.tbPlayer3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbPlayer3.Name = "tbPlayer3";
             this.tbPlayer3.ReadOnly = true;
-            this.tbPlayer3.Size = new System.Drawing.Size(206, 35);
+            this.tbPlayer3.Size = new System.Drawing.Size(113, 27);
             this.tbPlayer3.TabIndex = 2;
             // 
             // tbPlayer4
             // 
-            this.tbPlayer4.Location = new System.Drawing.Point(110, 304);
-            this.tbPlayer4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tbPlayer4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbPlayer4.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPlayer4.Location = new System.Drawing.Point(59, 152);
+            this.tbPlayer4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbPlayer4.Name = "tbPlayer4";
             this.tbPlayer4.ReadOnly = true;
-            this.tbPlayer4.Size = new System.Drawing.Size(206, 35);
+            this.tbPlayer4.Size = new System.Drawing.Size(113, 27);
             this.tbPlayer4.TabIndex = 3;
             // 
             // btnGameStart
             // 
+            this.btnGameStart.BackColor = System.Drawing.Color.Moccasin;
             this.btnGameStart.Enabled = false;
-            this.btnGameStart.Location = new System.Drawing.Point(354, 300);
-            this.btnGameStart.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnGameStart.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnGameStart.Location = new System.Drawing.Point(191, 150);
+            this.btnGameStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnGameStart.Name = "btnGameStart";
-            this.btnGameStart.Size = new System.Drawing.Size(181, 45);
+            this.btnGameStart.Size = new System.Drawing.Size(99, 29);
             this.btnGameStart.TabIndex = 4;
             this.btnGameStart.Text = "게임 시작";
-            this.btnGameStart.UseVisualStyleBackColor = true;
+            this.btnGameStart.UseVisualStyleBackColor = false;
             this.btnGameStart.Click += new System.EventHandler(this.btnGameStart_Click);
             // 
             // cbGame
             // 
+            this.cbGame.Font = new System.Drawing.Font("SUIT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.cbGame.FormattingEnabled = true;
             this.cbGame.Items.AddRange(new object[] {
             "Yacht Dice",
             "Knucklebone"});
-            this.cbGame.Location = new System.Drawing.Point(354, 69);
+            this.cbGame.Location = new System.Drawing.Point(191, 34);
+            this.cbGame.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbGame.Name = "cbGame";
-            this.cbGame.Size = new System.Drawing.Size(181, 32);
+            this.cbGame.Size = new System.Drawing.Size(99, 28);
             this.cbGame.TabIndex = 6;
             // 
             // Lobby
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 419);
+            this.ClientSize = new System.Drawing.Size(306, 210);
             this.Controls.Add(this.cbGame);
             this.Controls.Add(this.btnGameStart);
             this.Controls.Add(this.tbPlayer4);
             this.Controls.Add(this.tbPlayer3);
             this.Controls.Add(this.tbPlayer2);
             this.Controls.Add(this.tbPlayer1);
-            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Lobby";
             this.Text = "Room";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Lobby_FormClosing);
